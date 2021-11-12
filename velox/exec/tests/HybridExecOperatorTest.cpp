ed3677ed3c8b5e511b8e3768d582100093a896d6
Ferdinand Xu <cheng.a.xu@intel.com>
2021-11-12 11:53:09 +0800

    Support Hybrid Executor (#3)

---
+++ velox/exec/tests/HybridExecOperatorTest.cpp
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "velox/dwio/dwrf/test/utils/BatchMaker.h"
+#include "velox/exec/tests/OperatorTestBase.h"
+#include "velox/exec/tests/PlanBuilder.h"
+#include "velox/core/HybridPlanNode.h"
+#include "velox/exec/HybridExecOperator.h"
+#include "velox/exec/Driver.h"
+#include <folly/init/Init.h>
+
+
+using namespace facebook::velox;
+using namespace facebook::velox::core;
+using namespace facebook::velox::exec;
+using namespace facebook::velox::exec::test;
+
+using facebook::velox::test::BatchMaker;
+
+class HybridExecOperatorTest : public OperatorTestBase {
+ protected:
+  void assertFilter(
+      std::vector<RowVectorPtr>&& vectors,
+      const std::string& filter = "c1 % 10  > 0") {
+    auto plan = PlanBuilder().values(vectors).filter(filter).planNode();
+
+    assertQuery(plan, "SELECT * FROM tmp WHERE " + filter);
+  }
+
+  std::shared_ptr<const RowType> rowType_{
+      ROW({"c0", "c1", "c2", "c3"},
+          {BIGINT(), INTEGER(), SMALLINT(), DOUBLE()})};
+};
+
+TEST_F(HybridExecOperatorTest, filter) {
+  Operator::registerOperator(
+      [&](DriverCtx* ctx,
+          int32_t id,
+          const std::shared_ptr<const core::PlanNode>& node)
+          -> std::unique_ptr<HybridExecOperator> {
+        if (auto hybridOp =
+                std::dynamic_pointer_cast<const HybridPlanNode>(node)) {
+          return std::make_unique<HybridExecOperator>(id, ctx, hybridOp);
+        }
+        return nullptr;
+      });
+  std::vector<RowVectorPtr> vectors;
+  for (int32_t i = 0; i < 10; ++i) {
+    auto vector = std::dynamic_pointer_cast<RowVector>(
+        BatchMaker::createBatch(rowType_, 100, *pool_));
+    vectors.push_back(vector);
+  }
+  createDuckDbTable(vectors);
+
+  assertFilter(std::move(vectors));
+}
